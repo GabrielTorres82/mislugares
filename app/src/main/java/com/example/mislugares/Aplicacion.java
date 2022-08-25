15e6ff0818f3454b818d7cc8b8dfe1ae9f5848a3
GabrielTorres82 <gabriel.torres.solis82@gmail.com>
2022-08-25 20:42:36 +0200

    segunda prueba ya con todo

---
+++ app/src/main/java/com/example/mislugares/Aplicacion.java
@@ -0,0 +1,15 @@
+package com.example.mislugares;
+
+import android.app.Application;
+
+public class Aplicacion extends Application {
+    public RepositorioLugares lugares = new LugaresLista();
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+    }
+
+}
